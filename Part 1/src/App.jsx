c4f07c1eb335694ccb2e970babf0572f8b1c8838
Jesus Sarmiento <ing.jesussarmiento@gmail.com>
2025-02-07 17:47:46 -0500

    Part 1 ejercicios 1.3

--- Part 1/src/App.jsx
+++ Part 1/src/App.jsx
@@ -3,17 +3,18 @@ const Header = (props) => {
   
  return <h1>{props.title}</h1>
 }
-
+/*
 const Part = (props) =>{
 
   return <p> {props.title} {props.nejercicios}</p>
-}
+}*/
 const Content = (props) => {
   return (
   <div>
-    <Part title = {props.title[0]} nejercicios = {props.ejercicios[0]}></Part>
+  <p> {props.title} {props.ejercicios}</p>
+    {/* <Part title = {props.title[0]} nejercicios = {props.ejercicios[0]}></Part>
     <Part title = {props.title[1]} nejercicios = {props.ejercicios[1]}></Part>
-    <Part title = {props.title[2]} nejercicios = {props.ejercicios[2]}></Part>
+    <Part title = {props.title[2]} nejercicios = {props.ejercicios[2]}></Part> */}
   </div>
   )
 
@@ -29,7 +30,9 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const titulo = ['Fundamentals of React','Using props to pass data','State of a component']
+  const part1 =  {name:'Fundamentals of React', exercises: 10 }
+  const part2 = {name:'Using props to pass data', exercises: 7}
+  const part3 = {name:'State of a component', exercises: 14}
   const exercises = [10,7,14]
   //const sum = exercises[0]+exercises[1]+exercises[2]
 
@@ -37,7 +40,9 @@ const App = () => {
     <div>
       <Header title = {course}></Header>
 
-      <Content title ={titulo} ejercicios={exercises}></Content>
+      <Content title ={part1.name} ejercicios={part1.exercises}></Content>
+      <Content title ={part2.name} ejercicios={part2.exercises}></Content>
+      <Content title ={part3.name} ejercicios={part3.exercises}></Content>
   
       
       <Total ejercicios={exercises}></Total>
